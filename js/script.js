a24eab5c70912dd958449f26b8d5dc75a7bf83c4
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-03-29 15:02:18 +0000

    ...

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 // Copyright (c) 2022 Angelo Pintilie All rights reserved
 //
 // Created by: Angelo Pintilie
-// Created on: Sep 2022
+// Created on: Mar 2022
 // This file contains the JS functions for index.html
 
 function enterClicked () {
   // this function gets user's first name and age and displays it back to user
 
   // get first name from text field
-  let firstName = document.getElementById("first-name").value
+  let firstName = document.getElementById("hours").value
 	// get age from text field and cast it to integer
-  let userAge = parseInt(document.getElementById("age-entered").value)
-
+  let userAge = parseInt(document.getElementById("pay-entered").value)
   // display name and age back to user
-  document.getElementById('user-info').innerHTML = "Your street name is " + firstName + " and your street number is " + userAge + " ."
+  document.getElementById('user-info').innerHTML = "Your pay will be " + firstName + " and the government is taking " + userAge + " ."
 }
