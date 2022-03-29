f06c4134c92cccf0dec41697edcfecba74f3a7f8
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-03-29 14:33:00 +0000

    recycling code

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Angelo Pintilie All rights reserved
+//
+// Created by: Angelo Pintilie
+// Created on: Sep 2022
+// This file contains the JS functions for index.html
+
+function enterClicked () {
+  // this function gets user's first name and age and displays it back to user
+
+  // get first name from text field
+  let firstName = document.getElementById("first-name").value
+	// get age from text field and cast it to integer
+  let userAge = parseInt(document.getElementById("age-entered").value)
+
+  // display name and age back to user
+  document.getElementById('user-info').innerHTML = "Your street name is " + firstName + " and your street number is " + userAge + " ."
+}
