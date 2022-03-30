e31fe69dc5dafcfa42166a4f9d388725681da940
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-03-30 03:09:08 +0000

    minimal organizing

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
 // This file contains the JS functions for index.html
 
 function enterClicked () {
-  // this function gets user's first name and age and displays it back to user
+  // this function gets user's hours worked and hourly pay, then calculates their total pay including tax.
 
-  // get first name from text field
-  let firstName = document.getElementById("hours").value
-	// get age from text field and cast it to integer
-  let userAge = parseInt(document.getElementById("pay-entered").value)
-  // display name and age back to user
-  document.getElementById('user-info').innerHTML = "Your pay will be " + firstName + " and the government is taking " + userAge + " ."
+  // get hours from text field
+  let hours = document.getElementById("hours").value
+	// get hourly pay from text field and cast it to integer
+  let payEntered = parseInt(document.getElementById("pay-entered").value)
+  // display pay and tax back to user
+  document.getElementById('user-info').innerHTML = "Your pay will be " + hours + " and the government is taking " + payEntered + " ."
 }
